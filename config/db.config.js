e0e28189cbd698e0f78458cd445d3e1e16f33f04
teslasdev <Teslasdeveloper@gmail.com>
2023-11-19 15:33:09 +0100

    start initial

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-   HOST: "localhost",
-   USER: "root",
-   PASSWORD: "",
-   DB: "umecarts",
+   HOST: "db-mysql-nyc3-18481-do-user-10286398-0.c.db.ondigitalocean.com",
+   USER: "doadmin",
+   PASSWORD: "AVNS_17f5CtDALkQtu6Xz2jn",
+   DB: "defaultdb",
    dialect: "mysql",
    pool: {
      max: 5,
      min: 0,
      acquire: 30000,
      idle: 10000
-   }
+   },
+   port : "25060"
  };
